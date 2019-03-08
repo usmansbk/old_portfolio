73ac3c42b214237ae19431f989ad28b3a2a9ae22
Usman Suleiman <usmansbk@gmail.com>
2019-03-08 01:02:23 +0100

    Refactor home

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
+import Brightness4 from '@material-ui/icons/Brightness4';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+const styles = {
+  grow: {
+    flexGrow: 1
+  },
+  title: {
+    marginLeft: 8
+  }
+};
+
+function App({ classes }) {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Avatar alt="Babakolo Usman Suleiman" src="https://avatars3.githubusercontent.com/u/10219539?s=460&v=4" />
+          <Typography className={[classes.grow, classes.title]} variant="h4" color="inherit" noWrap>
+            Raw blog
+          </Typography>
+          <IconButton color="inherit">
+            <Brightness4 />
+          </IconButton>
+          <IconButton color="inherit" aria-label="Twitter">
+            <SvgIcon viewBox="0 0 20 20">
+            <path d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"></path>
+            </SvgIcon>
+          </IconButton>
+          <IconButton color="inherit" aria-label="GitHub">
+            <SvgIcon>
+              <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"></path>
+            </SvgIcon>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default withStyles(styles)(App);
